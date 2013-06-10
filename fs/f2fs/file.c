9bd7ab3a314b7793c1ed331732160f0a7aab0539
Alejandro Martinez Ruiz <alex@nowcomputing.com>
2013-06-10 12:37:13 +0200

    f2fs: return to old style ->fsync()
    Upstream commit 02c24a821 fs: push i_mutex and filemap_write_and_wait
    down into ->fsync() handlers changed the implementation of fsync so that
    it is now responsible for locking i_mutex and calling
    filemap_write_and_wait_range().
    
    Remove those from f2fs for pre-3.1 kernels.
    
    Signed-off-by: Alejandro Martinez Ruiz <alex@nowcomputing.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -138,7 +138,7 @@ static int get_parent_ino(struct inode *inode, nid_t *pino)
 	return 1;
 }
 
-int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
+int f2fs_sync_file(struct file *file, int datasync)
 {
 	struct inode *inode = file->f_mapping->host;
 	struct f2fs_sb_info *sbi = F2FS_SB(inode->i_sb);
@@ -154,16 +154,9 @@ int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		return 0;
 
 	trace_f2fs_sync_file_enter(inode);
-	ret = filemap_write_and_wait_range(inode->i_mapping, start, end);
-	if (ret) {
-		trace_f2fs_sync_file_exit(inode, need_cp, datasync, ret);
-		return ret;
-	}
 
 	/* guarantee free sections for fsync */
 	f2fs_balance_fs(sbi);
-
-	mutex_lock(&inode->i_mutex);
 
 	if (datasync && !(inode->i_state & I_DIRTY_DATASYNC))
 		goto out;
@@ -204,7 +197,6 @@ int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		ret = blkdev_issue_flush(inode->i_sb->s_bdev, GFP_KERNEL, NULL);
 	}
 out:
-	mutex_unlock(&inode->i_mutex);
 	trace_f2fs_sync_file_exit(inode, need_cp, datasync, ret);
 	return ret;
 }
