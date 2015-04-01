7354ebf9989307f0fd2604fc56a7809539352021
Andrea Mennillo <handymenny@outlook.com>
2015-04-01 18:28:01 +0200

    zram: add snappy algorithm backend
    Change-Id: I48d6e328a1e30bc9024a4b903c0ad9f536eebd7b
    
    Signed-off-by: Andrea Mennillo <handymenny@outlook.com>

---
+++ drivers/block/zram/zcomp_snappy.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2014 Sergey Senozhatsky.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ *
+ * Based on zcomp_lzo.h
+ *
+ */
+
+#ifndef _ZCOMP_SNAPPY_H_
+#define _ZCOMP_SNAPPY_H_
+
+#include "zcomp.h"
+
+extern struct zcomp_backend zcomp_snappy;
+
+#endif /* _ZCOMP_SNAPPY_H_ */
